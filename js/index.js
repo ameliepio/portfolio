18d32cbd7ae2abe95758929bf7d1da1a48cc26fe
Amelie <a.pioger@outlook.fr>
2017-10-09 16:14:22 +0200

    css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){               
+$(document).ready(function(){
   $('.materialboxed').materialbox();
 });
 $(document).ready(function(){
   $('.materialboxed').materialbox();
 });
+ // copy
+ $(function() {
+    $('.scroll-down').click (function() {
+      $('html, body').animate({scrollTop: $('portfolio').offset().top }, 'slow');
+      return false;
+    });
+  });
